5c848104fa7f36592d0092f6ed8f65b4914656ba
OriChaiMatan <ori.chaimatan@gmail.com>
2024-08-09 19:41:14 +0300

    Set basic count down

--- src/app/app-root/app.component.ts
+++ src/app/app-root/app.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-angular';
-  
+  handleCountdownDone() {
+    console.log('Countdown is done!');
+    // Add additional logic here, e.g., updating UI, alerting the user, etc.
+  }
 }
--- src/app/cmps/count-down/count-down.component.ts
+++ src/app/cmps/count-down/count-down.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Count } from '../../models/count.model';
+
 
 @Component({
   selector: 'count-down',
   templateUrl: './count-down.component.html',
-  styleUrl: './count-down.component.scss'
+  styleUrls: ['./count-down.component.scss']
 })
-export class CountDownComponent {
 
+export class CountDownComponent implements OnInit {
+  @Input() startFrom: number = 10;  // Default value is 10
+  @Output() onDone: EventEmitter<void> = new EventEmitter<void>();
+
+  counter: Count = {
+    count: 0
+  };
+
+
+  ngOnInit(): void {
+    this.counter = { count: this.startFrom };  // Initialize counter as an object
+    this.startCountdown();
+  }
+
+  startCountdown() {
+    const interval = setInterval(() => {
+      if (this.counter.count > 0) {
+        this.counter.count--;  // Decrement the count property
+      } else {
+        clearInterval(interval);
+        this.onDone.emit();  // Emit event when countdown is complete
+      }
+    }, 1000);  // Adjust the interval as needed (1000ms = 1s)
+  }
 }
