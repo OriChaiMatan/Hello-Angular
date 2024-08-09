76f887aa300c74d7d6cad1e4ae1feceb172822b2
OriChaiMatan <ori.chaimatan@gmail.com>
2024-08-09 12:34:14 +0300

    Added Count-Down cmp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app-root/app.component';
 import { AnimalListComponent } from './cmps/animal-list/animal-list.component';
 import { SeasonClockComponent } from './cmps/season-clock/season-clock.component';
+import { CountDownComponent } from './cmps/count-down/count-down.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnimalListComponent,
-    SeasonClockComponent
+    SeasonClockComponent,
+    CountDownComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cmps/count-down/count-down.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'count-down',
+  templateUrl: './count-down.component.html',
+  styleUrl: './count-down.component.scss'
+})
+export class CountDownComponent {
+
+}
